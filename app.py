d79f5cdbb0be2d732af5ab1088512996a68227d6
unknown <anirudhnandala8@gmail.com>
2026-05-12 19:43:47 +0530

    fixed port binding for render

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 import pandas as pd
 import joblib
@@ -78,4 +79,5 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
